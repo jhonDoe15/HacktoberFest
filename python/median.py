cbf98815c77c403a189a309d5b0ce163b686a8f7
jhonDoe15 <62393042+jhonDoe15@users.noreply.github.com>
2020-10-28 22:23:30 +0200

    Update median.py
    fixed commenting

--- python/median.py
+++ python/median.py
@@ -1,10 +1,10 @@
-/*
-Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
 
-Follow up: The overall run time complexity should be O(log (m+n)).
+# Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+
+# Follow up: The overall run time complexity should be O(log (m+n)).
 
  
- */
+ 
  
  class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
